189e692904c2ff95fbe6169a52025e88170e72a0
AraceliGS <aragutarras@gmail.com>
2017-11-19 00:22:18 -0500

    añadiendo la condición qque me permitirá habilitar y deshabilitar mi botón

--- js/app.js
+++ js/app.js
@@ -1,17 +1,21 @@
 // Para que estos eventos ocurran cuando la página haya cargado completamente, uso el evento load
 
 window.addEventListener('load', function() {
-    var button = document.querySelector('button');
-    var textarea = document.querySelector('textarea');  
-  
-    button.addEventListener('click', function() {
+  var button = document.querySelector('button');
+  var textarea = document.querySelector('textarea'); 
+  button.addEventListener('click', function() {
+    // Condición if else que me permitirá habilitar y desabilitar mi botón dependiendo si tienen contenido o no 
+    if (textarea.value !== '') {
       var sectionDivs = document.getElementsByTagName('section')[0];
-      var div = document.createElement('div');
-      var message = textarea.value;  
+      var div = document.createElement('div'); 
+      var message = textarea.value; 
       var text = document.createTextNode(message);
       div.appendChild(text);    
       sectionDivs.appendChild(div);
-      div.classList.add('divText');
-    });
+      div.classList.add('divText');  
+    } else {
+      button.setAttribute('disabled', 'true');
+    }
   });
+});
   
